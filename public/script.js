41b95ef97d4a48e2579bc127aa8fd89cd3112860
rolandarau <rolanda763@gmail.com>
2019-06-24 22:34:55 +0300

    Prideta gerimai, kuriu kaina 2eur

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const table = document.getElementById('table');
 let sumVal = 0;
 for (let i = 1; i < table.rows.length; i++) {
     if (i !== 5 && i !== 6 && i !== 7) {
-        sumVal = sumVal + parseFloat(table.rows[i].cells[3].innerHTML);
+        sumVal = sumVal + parseFloat(table.rows[i].cells[2].innerHTML);
     }
 }
 document.getElementById('val').innerHTML = 'Iš viso suprekiauta (šerbetas sirupais): ' + sumVal + ' &euro;';
@@ -12,7 +12,7 @@ const table1 = document.getElementById('table');
 let sumVal1 = 0;
 for (let i = 1; i < table.rows.length; i++) {
     if (i !== 4) {
-        sumVal1 = sumVal1 + parseFloat(table.rows[i].cells[3].innerHTML);
+        sumVal1 = sumVal1 + parseFloat(table.rows[i].cells[2].innerHTML);
     }
 }
 document.getElementById('val1').innerHTML = 'Iš viso suprekiauta (šerbetas stiklinėmis): ' + sumVal1 + ' &euro;';
@@ -24,7 +24,7 @@ Object.entries(tab).forEach((el) => {
     const table = el[1];
     let sum = 0
     for (let i = 1; i < table.rows.length; i++) {
-        sum = sum + parseFloat(table.rows[i].cells[3].innerHTML);
+        sum = sum + parseFloat(table.rows[i].cells[2].innerHTML);
     }
     copy.push(sum);
 });
